36370b418fba336420701e845d6e036e7bd53da4
MesopotamiaAlpha <leao-gomes@hotmail.com>
2016-08-02 10:09:14 -0300

    Initial commit

---
+++ app/src/main/java/exemplo/webmobile/android/conexaoexterna/DB.java
@@ -0,0 +1,96 @@
+package exemplo.webmobile.android.conexaoexterna;
+
+/*
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+*/
+import android.util.Log;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
+
+/**
+ * Created by evandro on 16/03/2015.
+ */
+public class DB extends _Default implements Runnable {
+
+    private Connection conn;
+    private String host = "192.168.1.37";
+    private String db = "android";
+    private int port = 3306;
+    private String user = "solaruser";
+    private String pass = "mesopotamia45";
+    private String url = "jdbc:mysql://%s:%d/%s/";
+
+    public DB() {
+        super();
+        this.url = String.format(this.url,this.host, this.port, this.db);
+
+        this.conecta();
+        this.disconecta();
+    }
+
+    @Override
+    public void run() {
+        try{
+            //original Class.forName("org.postgresql.Driver");
+            Class.forName("com.mysql.jdbc.Driver");
+            this.conn = DriverManager.getConnection(this.url,this.user,this.pass);
+        }catch (Exception e){
+            this._mensagem = e.getMessage();
+            this._status = false;
+        }
+    }
+
+    private void conecta(){
+        Thread thread = new Thread(this);
+        thread.start();
+        try{
+            thread.join();
+        }catch (Exception e){
+            this._mensagem = e.getMessage();
+            this._status = false;
+        }
+    }
+
+    private void disconecta(){
+        if (this.conn!= null){
+            try{
+                this.conn.close();
+            }catch (Exception e){
+
+            }finally {
+                this.conn = null;
+            }
+        }
+    }
+
+    public ResultSet select(String query){
+        this.conecta();
+        ResultSet resultSet = null;
+        try {
+            resultSet = new ExecuteDB(this.conn, query).execute().get();
+        }catch (Exception e){
+            this._status = false;
+            this._mensagem = e.getMessage();
+        }
+        return resultSet;
+    }
+
+    public ResultSet execute(String query){
+        this.conecta();
+        ResultSet resultSet = null;
+        try {
+            resultSet = new ExecuteDB(this.conn, query).execute().get();
+        }catch (Exception e){
+            this._status = false;
+            this._mensagem = e.getMessage();
+        }
+        return resultSet;
+    }
+
+}
